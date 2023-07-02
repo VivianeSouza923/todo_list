670d3dab065d91f5855eacc5a34b35f94291662d
Vivi <viviane.souza05@aluno.ifce.edu.br>
2023-07-02 10:56:16 -0300

    today's work

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -26,13 +26,28 @@ class TodoListPage extends StatelessWidget {
                 // outro exemplo de borda que eu gosto mais, aquela que fica tipo voando, bem clean
                 border: InputBorder.none,
                 // especificar uma mensagem de erro:
-                errorText: "Campo Obrigatório!"
+                errorText: "Campo Obrigatório!",
+                // botar um texto fixo, UM PREFIXO? tipo R$ de preço, isso nunca muda, certo?
+                //    ---- >      prefixText: "R\$ ",    < ----
+                // QUERO UM SUFIXO NO MEU TEXTO?
+                //    ---- >            suffixText: "cm",      < ----
+
                 ),
 
                 // esconder caracteres, tipo codificar, botar asteriscos, sca? É BOM PARA SENHAS.
                 obscureText: true,
-                // posso escolher como codificar os meus textos, números etc. Nesse caso, escolho codificá-los como *
-                obscuringCharacter: "*",
+                // posso escolher como codificar os meus textos, números etc. Nesse caso, escolho codificá-los como $
+                // como o cifrão tem um trabalho já dentro do Flutter, que é pegar uma variável e colocar no meio de um texto, para eu exibi-lo como caractere mesmo, eu tenho que botar uma barra invertida antes dele.
+                // não funciona só botar o cifrão dentro das aspas, tem que ser "\$"
+                obscuringCharacter: "\$",
+                //quero mudar meu teclado somente para NUMÉRICO:
+                //keyboardType: TextInputType.number,
+                // quero o meu teclado com aquele formato para quem vai digitar e-mail, para facilitar:
+                keyboardType: TextInputType.emailAddress,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.green,
+                ),
           ),
         ),
       ),
